f84769f3676e40c18192b30fdc13c370eda46ca7
BKsher <derfer2003@gmail.com>
2022-05-23 02:29:00 +0300

    Добавьте файлы проекта.

---
+++ Laba2(Jonson, Deikstra)/Laba2(Jonson, Deikstra).cpp
@@ -0,0 +1,30 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+struct edge {
+	int a, b, cost;
+};
+
+int n, m, v;
+vector<edge> e;
+const int INF = 1000000000;
+
+void ford_belman(vector<edge> e) {
+	vector<int> d(n, INF);
+	d[v] = 0;
+	while (true) {
+		bool any = false;
+		for (int j = 0; j < e.size(); ++j)
+			if (d[e[j].a] < INF)
+				if (d[e[j].b] > d[e[j].a] + e[j].cost) {
+					d[e[j].b] = d[e[j].a] + e[j].cost;
+					any = true;
+				}
+		if (!any)  break;
+	}
+}
+
+int main()
+{
+    
+}
